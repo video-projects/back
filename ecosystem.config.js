8ee15adaff1866848f2a78e8b8c8ff0d7b1d2c3f
mahankabir <mahan.kabir@gmail.com>
2025-07-08 20:06:26 +0330

    feat(dev): was updated.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'video_back',
-      script: 'src/main.ts',
-      interpreter: 'ts-node',
-      watch: true,
+      script: 'dist/main.js',
+      watch: false,
     },
   ],
 };
